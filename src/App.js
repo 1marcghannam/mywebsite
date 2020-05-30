6043c0d45b1e3dd3a413b79ce81af877ea614f49
heavymg2000 <46615105+heavymg2000@users.noreply.github.com>
2020-05-30 14:54:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Nav.css';
+import Navigation from "./Navigation";
+import Home from "./Home";
+import Home2 from "./Home2";
+import Footer from "./Footer";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='test'>
+          <Navigation/>
+          <Home/>
+
+
+          <Footer/>
+
+
+
+      </div>
+
+
+
+
+
   );
 }
 
