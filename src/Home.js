81c624577c1c66d6a810a440c15cd86d5abc0870
heavymg2000 <46615105+heavymg2000@users.noreply.github.com>
2020-06-01 17:55:32 -0400

    monday

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Home.css'
 import Face from './static/face.jpeg';
 import {Link} from "react-router-dom";
+import computer from "./static/computer.png";
 
 
 
@@ -16,21 +17,10 @@ function Home() {
         <div className='helloMessage'>
 
             <h1 className='intro pulse'>"Hello World!"</h1>
-            <p className='intro-sub'>My name is Marc. Welcome to my page! </p>
-            <h1 className='intro-sub3'>A little bit about me</h1>
-            <p className='story'> I'm a Junior Computer Engineering Student attending Wentworth Institute of Technology.  </p>
-            <p className='story'> I'm a passionate learner and problem solver. I'm also a watch collector and enthusiast. </p>
+            <img className='computer' src={computer}/>
+            <p className='intro-sub'>My name is Marc. Welcome to my page!</p>
 
         </div>
-            <div className='helloMessage2'>
-                <Link to='contact'>
-                <p className='contact'>Contact Me!</p>
-                </Link>
-
-
-
-
-            </div>
 
 
 
--- src/Navigation.js
+++ src/Navigation.js
@@ -20,31 +20,20 @@ window.addEventListener("resize", () => {
 
 
     const navSlide = () => {
-
         const burger = document.querySelector('.burger');
 
 
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+        const nav = document.querySelector('.nav-links');
 
 
 
 
-        //Toggle nav
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('.toggle');
-        navLinks.forEach((link, index) =>{
-
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
-                console.log(index / 7);
-            }
-        });
+            //Toggle nav
+            nav.classList.toggle('nav-active');
+            burger.classList.toggle('.toggle');
 
 
-}
+    }
 
 
 
